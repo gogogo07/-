06a7b7322ba5df7d8f378152efcb4fd14af072a5
ouyuj <1252809618@qq.com>
2019-11-20 13:13:24 +0800

    2019-11-20 换了一种方式爬取专业信息

--- get_info/get_major_info.py
+++ get_info/get_major_info.py
@@ -1,44 +1,37 @@
-import time
-import re
+import requests
 
-from selenium import webdriver
 from concurrent.futures import ProcessPoolExecutor
 from sql_server.sql_server_class import SqlServer
 
 
-def get_info_major(url, sql, pages, year):
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:70.0) Gecko/20100101 Firefox/70.0'
+}
+items = ['name', 'spname', 'local_province_name',
+         'local_type_name', 'year', 'local_batch_name', 'average', 'min']
+
+
+def get_info_major(start, url, sql, pages, year):
+    sql = sql.format(year)
     sql_server = SqlServer('course_design')
-    driver = webdriver.Firefox()
-    driver.get(url)  # 打开网页
-    time.sleep(2)
-    m = driver.find_elements_by_class_name('more-search')[1]
-    m.find_element_by_tag_name('span').click()
-    i = 0
     con, cur = sql_server.connect_and_cursor
-    while True:
-        i += 1
-        message = driver.find_element_by_class_name('search-table')
-        pattern = re.compile(r'(.*)\n+')
-        message1 = pattern.findall(message.text + '\n')
-        data = [tuple(message1[i].split()) for i in range(1, len(message1))]
-        print(year, i, data[0][0])
-        cur.executemany(sql, data)
+    for i in range(start, pages + 1):
+        tmp_url = url.format(i, year)
+        res = requests.get(tmp_url, headers=headers)
+        data = res.json()['data']['item']
+        tmp_data = []
+        for d in data:
+            tmp = []
+            for item in items:
+                tmp.append(d[item])
+            tmp_data.append(tuple(tmp))
+        cur.executemany(sql, tmp_data)
         con.commit()
-        mes = driver.find_element_by_class_name("fypages")
-        mes.find_elements_by_tag_name('li')[9].click()
-        if i == pages:
-            print('爬取成功')
-            break
+        print(year, i, tmp_data[0][0])
 
 
 if __name__ == '__main__':
-    url1 = r'https://gkcx.eol.cn/linespecialty?schoolyear=2018'
-    sql1 = 'insert into major_info2018 values(%s, %s, %s, %s, %s, %s, %s, %s)'
-    url2 = r'https://gkcx.eol.cn/linespecialty?schoolyear=2017'
-    sql2 = 'insert into major_info2017 values(%s, %s, %s, %s, %s, %s, %s, %s)'
-    url3 = r'https://gkcx.eol.cn/linespecialty?schoolyear=2016'
-    sql3 = 'insert into major_info2016 values(%s, %s, %s, %s, %s, %s, %s, %s)'
-    with ProcessPoolExecutor(3) as pool:
-        pool.submit(get_info_major, url1, sql1, 25977, 2018)
-        pool.submit(get_info_major, url2, sql2, 26375, 2017)
-        pool.submit(get_info_major, url3, sql3, 22134, 2016)
+    # 11340
+    sql = 'insert into major_info{} values(%s, %s, %s, %s, %s, %s, %s, %s)'
+    url = 'https://api.eol.cn/gkcx/api/?access_token=&admissions=&central=&department=&dual_class=&f211=&f985=&is_dual_class=&keyword=&local_batch_id=&local_type_id=&page={}&province_id=&school_type=&signsafe=&size=20&type=&uri=apidata/api/gk/score/special&year={}'
+    get_info_major(17215, url, sql, 25977, 2018)
