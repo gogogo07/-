64e48869f6d6de40707b7ba8de2830b129cab583
ouyuj <1252809618@qq.com>
2019-11-26 09:10:58 +0800

    2019-11-26 添加了连接服务器mysql的类

---
+++ mysql/mysql_class.py
@@ -0,0 +1,55 @@
+import pymysql
+import json
+import os
+
+
+class MysqlConnect:
+    """自定义一个连接MySql的类"""
+
+    def __init__(self, database, is_close_auto=True):
+        self._database = database
+        self._connect = None
+        self._cursor = None
+        self._is_close_auto = is_close_auto
+        self.connect_to_mysql()
+
+    def connect_to_mysql(self):
+        with open(os.path.dirname(os.getcwd()) + '\\mysql\\config.json', 'r') as f:
+            config = json.load(f)
+        self._connect = pymysql.connect(database=self._database, **config)
+        if self._connect:
+            self._cursor = self.connect.cursor()
+
+    @property
+    def connect(self):
+        return self._connect
+
+    @property
+    def connect_and_cursor(self):
+        return self._connect, self._cursor
+
+    def creat_table(self, table):
+        pass
+
+    def insert(self, table, *args):
+        pass
+
+    def __del__(self):
+        if self._is_close_auto:
+            if self._cursor:
+                print('cursor close')
+                self._cursor.close()
+            if self._connect:
+                print('connect close')
+                self._connect.close()
+
+
+if __name__ == '__main__':
+    with open(os.path.dirname(os.getcwd()) + '\\mysql\\config.json', 'r') as f:
+        config = json.load(f)
+    d = ['厦门大学']
+    con = pymysql.connect(database='course_design', **config)
+    cur = con.cursor()
+    cur.execute(f"select s_id from schools where s_name = '{d[0]}'")
+    res = cur.fetchone()
+    print(res)
