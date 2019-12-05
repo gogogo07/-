3f54d16e134dd0926839604f71cce7bdbbe01eb3
Astralll <lsffann@163.com>
2019-12-05 14:12:41 +0800

    Update CallMainWindow.py
    暂时先保存一下

--- QT/CallMainWindow.py
+++ QT/CallMainWindow.py
@@ -2,11 +2,10 @@
 import pymysql
 from PyQt5.Qt import *
 import sys
-
+#coding=utf-8
+import re
 from PyQt5.QtGui import QStandardItemModel, QStandardItem
 from PyQt5.QtWidgets import QTableWidgetItem, QMessageBox, QMainWindow, QApplication
-from pymssql import DataError, ProgrammingError
-
 from QT import ShowWindow
 # 继承至界面文件的主窗口类
 from QT.ShowWindow import Ui_MainWindow
@@ -23,7 +22,13 @@ config = {
 
 class MyMainWindow(QMainWindow, Ui_MainWindow):
 
-
+    """
+    构造函数初始化
+    Attributes:
+        self.db: 连接数据库
+        self.cur：与数据库建立的连接关系
+        self.model:表格模型，与UI界面的表格绑定，用于显示信息
+    """
     def __init__(self, parent=None):
         super(MyMainWindow, self).__init__(parent)
         self.setupUi(self)
@@ -42,8 +47,16 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
     添加查询报错相应机制
     未来优化可以加个翻页切换
     TODOOOOOOOOOOOOO设置窗口关闭时断开数据库连接
+    
+    执行SQL语句在数据库中获得结果
+    Args:
+        sql:要执行的SQL语句
+    Returns:
+        results:查询的结果
+        -1：当执行出错时返回标识符-1
+    Raises:
+        EXception:以防万一，一旦出现任何错就报错。
     '''
-    # 查找数据库数据
     def search(self, sql):
         try:
             self.cur.execute(sql)
@@ -52,7 +65,14 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
         except Exception:
             return -1
 
-    # 获取列名
+    '''
+    获取列名
+    Returns:
+        colname:列名列表
+        -1：出错标识符-1
+    Raises:
+        Exception:以防万一，出现任何错都报错 
+    '''
     def get_colname(self):
         try:
             colname = [tup[0] for tup in self.cur.description]  # 获得列名
@@ -60,28 +80,76 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
         except Exception:
             return -1
 
-    # 获取输入的自然语言和SQL语句
+    '''
+    获取输入的自然语言和SQL语句
+    Attributes:
+        self.wquery:输入的自然语言
+        self.sqlquery:输入的SQL语句
+    '''
     def get_input(self):
         self.wquery = self.InQ_Text.toPlainText()      # 获取输入的自然语言
         self.sqlquery = self.InSql_Text.toPlainText()  # 获取sql语句
 
-    # 输入参数清空/重置
+    '''
+        获取输入的自然语言和SQL语句
+        Returns:
+            sqlkey:根据空格切割的字符串列表
+        '''
+    def findwkey(self):
+        pattern = re.compile(r"(.*?)")
+        wkey = pattern.findall(self.wquery)
+        print(wkey)
+        return wkey
+
+    '''
+    获取输入的自然语言和SQL语句
+    Returns:
+        sqlkey:根据空格切割的字符串列表
+    '''
+    def findsqlkey(self):
+        # 匹配查找‘关键词’
+        pattern = re.compile(r"'(.*?)'")
+        sqlkey = pattern.findall(self.sqlquery)
+        print("-----------------------")
+        print(sqlkey)
+        # 匹配查找"关键词"
+        pattern = re.compile(r"\"(.*?)\"")
+        temp1 = pattern.findall(self.sqlquery)
+        print("-----------------------")
+        print(temp1)
+        sqlkey = sqlkey + temp1
+        # 匹配查找 数字关键词
+        pattern = re.compile(r"[0-9]+")
+        temp1 = pattern.findall(self.sqlquery)
+        print("-----------------------")
+        print(temp1)
+        sqlkey = sqlkey + temp1
+        # 匹配查找 年份关键词
+        pattern = re.compile(r" .+\d+")
+        temp1 = pattern.findall(self.sqlquery)
+        print("-----------------------")
+        print(temp1)
+        sqlkey = sqlkey + temp1
+
+        print(sqlkey)
+        return sqlkey
+
+    '''
+    输入参数清空/重置
+    '''
     def init_input(self):
         self.wquery = ""    # 清空输入的自然语言
         self.Sqlquer = ""   # 清空输入的SQL语句
         self.model.clear()  # 清空上一次查询的结果
 
-    # 根据输入的SQL语言对比数据库查询函数
+    '''
+    根据输入的SQL语言对比数据库查询函数
+    并把结果显示到表格
+    Attributes:
+        self.rowsum:结果集（表格显示）总行数
+        self.colsum:结果集（表格显示）总列数
+    '''
     def search_sql(self):
-
-        # 如果输入为空，报错处理
-        if (len(self.wquery) == 0):
-            print(QMessageBox.information(self, "提醒", "未输入要查询的自然语言", QMessageBox.Yes, QMessageBox.Yes))
-            return
-        if(len(self.sqlquery) == 0):
-            print(QMessageBox.information(self, "提醒", "未输入要查询的SQL语句", QMessageBox.Yes, QMessageBox.Yes))
-            return
-
         # 执行对应的查询命令
         data = self.search(self.sqlquery)
         # 查询异常
@@ -109,13 +177,30 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
                 self.model.setItem(i, j, finaldata)
         self.SearchRes_Tab.setModel(self.model)             # 结果插入表
 
-
-    #查询按钮响应事件
+    '''
+    查询按钮响应事件
+    Attributes:
+        self.rowsum:结果集（表格显示）总行数
+        self.colsum:结果集（表格显示）总列数
+    '''
     def on_Ok_But_click(self):
         self.init_input()  # 重置
-        self.search_sql()
+        self.get_input()   # 获取输入的自然语言和SQL语句
 
-    # 重置按钮响应事件
+        # 如果输入为空，报错处理
+        if (len(self.wquery) == 0):
+            print(QMessageBox.information(self, "提醒", "未输入要查询的自然语言", QMessageBox.Yes, QMessageBox.Yes))
+            return
+        if (len(self.sqlquery) == 0):
+            print(QMessageBox.information(self, "提醒", "未输入要查询的SQL语句", QMessageBox.Yes, QMessageBox.Yes))
+            return
+
+        self.findsqlkey()  # 分析SQL中的关键词
+        # self.search_sql()  # 执行查询并显示到表格
+
+    '''
+    重置按钮响应事件，清空输入参数和输入显示框
+    '''
     def on_Reset_But_click(self):
         # 清空输入框
         self.InSql_Text.clear()
